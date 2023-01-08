32b7e9e9b9800ca773e786e4b0e86283d3b5bf76
Noah Bumgardner <noah@playgamecafe.com>
2023-01-07 19:46:03 -0600

    Connect Menu to 3 Levels via Level Select

---
+++ menus/LevelSelect.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	if $TitleContentContainer/ButtonsContainer/LevelButton01:
+		$TitleContentContainer/ButtonsContainer/LevelButton01.grab_focus()
+
+func _on_LevelButton01_pressed():
+	get_tree().change_scene("res://gameplay/Level01_Square4By4.tscn")
+
+func _on_LevelButton02_pressed():
+	get_tree().change_scene("res://gameplay/Level02_RingOf13.tscn")
+
+func _on_LevelButton03_pressed():
+	get_tree().change_scene("res://gameplay/Level03_RoadShapeTOf22.tscn")
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://menus/StartMenu.tscn")
